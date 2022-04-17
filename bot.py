a896863ff368bbeae63c0071514061b537769937
Dion <92658890+SeorangDion@users.noreply.github.com>
2022-04-17 14:50:52 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,122 @@
+# Copyright (C) 2022 GNU GENERAL PUBLIC LICENCE
+#
+# Licensed under the GNU GENERAL PUBLIC LICENCE, Version 3 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# Copyright (C) 2022 @SeorangDion for WhisperBot repo
+# FROM WhisperBot <https://github.com/SeorangDion/WhisperBot>
+# t.me/DionProjects & t.me/DionSupport
+# Don't remove this credits!
+
+
+from telethon import events, TelegramClient, Button
+import logging
+from telethon.tl.functions.users import GetFullUserRequest as us
+import os
+
+
+logging.basicConfig(level=logging.INFO)
+
+API_HASH = os.environ.get("API_HASH", None)
+API_ID = os.environ.get("API_ID", None)
+BOT_NAME = os.environ.get("BOT_NAME", None)
+BOT_USERNAME = os.environ.get("BOT_USERNAME", None)
+TOKEN = os.environ.get("TOKEN", None)
+
+bot = TelegramClient(
+        "Dion",
+        api_id=API_ID,
+        api_hash=API_HASH
+        ).start(
+                bot_token=TOKEN
+                )
+db = {}
+
+@bot.on(events.NewMessage(pattern="^[!?/]start$"))
+async def stsrt(event):
+    await event.reply(
+            f"**Heya, I am a {BOT_NAME}!**\nType /help to see how to use me!")
+
+
+@bot.on(events.NewMessage(pattern="^[!?/]help$"))
+async def stsrt(event):
+    await event.reply(
+            f"**• How to use me:\n\nClick the button below or type **{BOT_USERNAME} wspr <username> | <text>**\nExample: `{BOT_USERNAME} wspr @Xflzu | Hello!`",
+            buttons=[
+                [Button.switch_inline("Go Inline", query="")]
+                ]
+            )
+
+
+@bot.on(events.InlineQuery())
+async def die(event):
+    if len(event.text) != 0:
+        return
+    me = (await bot.get_me()).username
+    
+@bot.on(events.InlineQuery(pattern="wspr"))
+async def inline(event):
+    me = (await bot.get_me()).username
+    try:
+        inp = event.text.split(None, 1)[1]
+        user, msg = inp.split("|")
+    except IndexError:
+        await event.answer(
+                [], 
+                switch_pm=f"@{me} [UserID]|[Message]",
+                switch_pm_param="start"
+                )
+    except ValueError:
+        await event.answer(
+                [],
+                switch_pm=f"Give a message too!",
+                switch_pm_param="start"
+                )
+    try:
+        ui = await bot(us(user))
+    except BaseException:
+        await event.answer(
+                [],
+                switch_pm="Invalid User ID/Username",
+                switch_pm_param="start"
+                )
+        return
+    db.update({"user_id": ui.user.id, "msg": msg, "self": event.sender.id})
+    text = f"""
+A Whisper Has Been Sent
+To [{ui.user.first_name}](tg://user?id={ui.user.id})!
+Click The Below Button To See The Message!\n
+**Note:** __Only {ui.user.first_name} can open this!__
+    """
+    dn = event.builder.article(
+            title="Its a secret message!",
+            description="It's a secret message!",
+            text=text,
+            buttons=[
+                [Button.inline(" Show Message! ", data="wspr")]
+                ]
+            )
+    await event.answer(
+            [dn],
+            switch_pm="It's a secret message!",
+            switch_pm_param="start"
+            )
+
+
+@bot.on(events.CallbackQuery(data="wspr"))
+async def ws(event):
+    user = int(db["user_id"])
+    lol = [int(db["self"])]
+    lol.append(user)
+    if event.sender.id not in lol:
+        await event.answer("🔐 This message is not for you!", alert=True)
+        return
+    msg = db["msg"]
+    if msg == []:
+        await event.anwswer(
+                "Oops!\nIt's looks like message got deleted from my server!", alert=True)
+        return
+    await event.answer(msg, alert=True)
+
+print("Started Bot, by github.com/SeorangDion.")
+bot.run_until_disconnected()
