f8748d8cfbe3aad0f2cc3334a023c4886a772c3f
Dion <92658890+SeorangDion@users.noreply.github.com>
2022-04-20 15:06:45 +0700

    Create dion_config.py
    By @SeorangDion

---
+++ dion_config.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2022 @SeorangDion for WhisperBot repo
+# FROM WhisperBot <https://github.com/SeorangDion/WhisperBot>
+# t.me/DionProjects & t.me/DionSupport
+# Don't remove this credits!
+
+import os
+
+
+DIONAPI_HASH = "23c93aa64d16911f521bd0b16291af57"
+DIONAPI_KEY = 14624642
+DIONBOT_NAME = os.environ.get("BOT_NAME", None) # Your bot name, example: Dion Bot
+BOT_USERNAME = os.environ.get("BOT_USERNAME", None) # Your bot username with (@), example: @WhisperXRobot
+DION_TOKEN = os.environ.get("TOKEN", None) # Your token bot, get one from t.me/botfather
